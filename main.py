0ea68403b94d97a721a5ef5723cf1b573138783d
Egorov Evgeniy <egorovzhenya@gmail.com>
2022-01-25 17:38:37 +0400

    Second commit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from pprint import pprint
 import csv
 import re
+import pandas as pd
+
+pd.set_option('display.max_columns', None)
 
 
 def read(file):
@@ -8,23 +11,6 @@ def read(file):
         rows = csv.reader(f)
         contacts_list = list(rows)
         text(contacts_list)
-
-
-def write(data):
-    with open('phonebook.csv', 'w', encoding='utf-8') as f:
-        writer = csv.writer(f, delimiter=" ")
-        writer.writerows(data)
-
-
-def phones(data):
-    revised_phones = []
-    for item in data:
-        pattern2 = r'(\+7|8)[\s-]*\(*(\d{3})\)*[\s-]*(\d{3})[\s-]*(\d{2})[\s-]*(\d{2})([\s-]*\(*(доб\.\s\d+)\)*)*'
-        repl2 = r' +7(\2)\3-\4-\5'
-        result = re.sub(pattern2, repl2, item)
-        revised_phones.append(result.split())
-    # print(revised_phones)
-    write(revised_phones)
 
 
 def text(data):
@@ -36,16 +22,35 @@ def text(data):
         repl = r'\1, \2, \3, \4, \5, \6, \7'
         result = re.sub(pattern, repl, string)
         revised_text.append(result)
-    # pprint(revised_text)
     phones(revised_text)
 
 
-def joined_list(file):
-    with open(file, encoding='utf-8') as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            pprint(row)
+def phones(data):
+    revised_phones = []
+    for item in data:
+        pattern2 = r'(\+7|8)[\s-]*\(*(\d{3})\)*[\s-]*(\d{3})[\s-]*(\d{2})[\s-]*(\d{2})([\s-]*\(*(доб\.\s\d+)\)*)*'
+        repl2 = r' +7(\2)\3-\4-\5'
+        result = re.sub(pattern2, repl2, item)
+        revised_phones.append(result.split())
+    write_draft(revised_phones)
+
+
+def grouped():
+    content = pd.read_csv('draft.csv')
+    group = content.groupby('lastname').first()
+    print(pd.DataFrame(group))
+
+
+def write_draft(data):
+    with open('draft.csv', 'w', encoding='utf-8') as f:
+        writer = csv.writer(f, delimiter=" ")
+        writer.writerows(data)
+
+
+def write_dict():
+    pass
 
 
 read('phonebook_raw.csv')
-joined_list("phonebook.csv")
+grouped()
+
